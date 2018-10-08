cc48e56cec8fb86edb3e72a87525c47cb800e379
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-08 17:17:02 +0200

    10

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
-var links = document.getElementsByTagName('a');
-for(i = 0; i < links.length; i++){
-    links[i].className = 'link-' + i;
+function getAverage (a,b,c,d) {
+    var average = (a+b*c-d) / 2;
+    console.log(average);
+    return average;
 }
+var myResult = getAverage(7, 12, 8, 3)
+console.log('the average is ' + myResult)
