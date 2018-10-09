fe62a0681887ef33ea156228da9aeb9589a16c95
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-09 17:39:52 +0200

    21

--- test.js
+++
@@ -1,8 +0,0 @@
-var colourChanger = document.getElementById('colour-changer');
-var colours = ['red', 'blue', 'green', 'pink'];
-var counter = 0;
-function changeColour (){
-    colourChanger.style.background = colours[counter];
-    counter++;
-}
-setInterval(changeColour,3000);  
