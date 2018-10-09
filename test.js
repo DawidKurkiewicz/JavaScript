d56936bfa0af13aeae6a74242b4f48c656d86611
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-09 13:52:08 +0200

    15

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
-var string1 = 'abc';
-var string2 = 'ABC';
+var myArray = new Array();
+myArray[0] = 8;
+myArray[1] = 'hello';
+var myCar = new Object();
+myCar.maxSpeed = 50
+myCar.driver = 'dawid'
 
-console.log(string1 === string2);
+myCar.drive = function(){console.log('now driving');};
+myCar.drive();
+var myCar2 = {maxSpeed: 50, driver: 'Net ninja', drive: function(speed, time){console.log(speed * time);} };
+console.log(myCar2.maxSpeed);
+myCar2.drive(50, 3);
