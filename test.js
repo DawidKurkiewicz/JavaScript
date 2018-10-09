dbe21ed6b583dcc0e50dbad080a73a34c7142c55
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-09 11:55:54 +0200

    11

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
 function getAverage (a,b,c,d) {
-    var average = (a+b*c-d) / 2;
+    var average = (a+b*c-d) / 2; //local variable
     console.log(average);
     return average;
 }
-var myResult = getAverage(7, 12, 8, 3)
-console.log('the average is ' + myResult)
+var myResult = getAverage(7, 12, 8, 3); //global variable
+
+
+
+function logResult() {
+    console.log('the avarage is ' +myResult + ' inside the function');
+}
+logResult();
